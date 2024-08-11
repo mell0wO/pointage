77a2d8ffadbd77aaa188d25d6534ade3ce22c8b8
mell0wO <160142288+mell0wO@users.noreply.github.com>
2024-08-11 16:00:58 +0100

    Update dashboard.py

--- point/dashboard.py
+++ point/dashboard.py
@@ -8,7 +8,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QCo
 from sqlalchemy import create_engine
 from matplotlib.dates import DateFormatter
 
-class MyWindow(QMainWindow):
+class Dashboard(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Jour de la Semaine vs Travail")
@@ -294,6 +294,6 @@ class MyWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MyWindow()
+    window = Dashboard()
     window.show()
     sys.exit(app.exec_())
