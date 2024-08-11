789c4bbf387a1e8866eb20bcf8368073edae09e0
mell0wO <160142288+mell0wO@users.noreply.github.com>
2024-08-11 16:52:51 +0100

    Update config.py

--- point/config.py
+++ point/config.py
@@ -1,12 +1,8 @@
 from configparser import ConfigParser
 
 def config(filename="db.ini", section="postgresql"):
-    # create a parser
     parser = ConfigParser()
-    # read config file
     parser.read(filename)
-
-    # get section, default to postgresql
     db = {}
     if parser.has_section(section):
         params = parser.items(section)
