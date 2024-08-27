1c4ff26dac6429fd440cb808c7afd7b6b5738a53
mell0wO <160142288+mell0wO@users.noreply.github.com>
2024-08-27 11:27:58 +0100

    Update dashboard.py

--- point/dashboard.py
+++ point/dashboard.py
@@ -153,6 +153,174 @@ class Dashboard(QWidget):
         except Exception as e:
             print(f"Erreur lors de l'application du filtre : {e}")
             self.filtered_df = pd.DataFrame()                
+import sys
+from matplotlib.table import Table
+import pandas as pd
+import matplotlib.pyplot as plt
+import mplcursors
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget, QListWidget, QPushButton, QHBoxLayout, QListWidgetItem
+from sqlalchemy import create_engine
+from matplotlib.dates import DateFormatter
+import seaborn as sns
+
+class Dashboard(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Jour de la Semaine vs Travail")
+        self.setGeometry(100, 100, 1200, 800)
+        self.layout = QVBoxLayout(self)
+        self.list_widget = QListWidget()
+
+        self.filter_layout = QHBoxLayout()
+        self.layout.addLayout(self.filter_layout)
+
+        self.month_listwidget = QListWidget()
+        self.month_listwidget.addItem("Tous les mois")
+        for month in range(1, 13):
+            self.month_listwidget.addItem(f"{month:02d}")
+        self.month_listwidget.setSelectionMode(QListWidget.MultiSelection)
+        self.filter_layout.addWidget(self.month_listwidget)
+
+        self.name_listwidget = QListWidget()
+        self.name_listwidget.addItem("Tous les noms")
+        self.name_listwidget.setSelectionMode(QListWidget.MultiSelection)
+        self.filter_layout.addWidget(self.name_listwidget)
+
+        self.apply_button = QPushButton("Appliquer le Filtre")
+        self.apply_button.clicked.connect(self.apply_filter)
+        self.filter_layout.addWidget(self.apply_button)
+        self.apply_button.setStyleSheet("""
+            QPushButton {
+            background-color: #1887f5;
+            color: white;
+            border: none;
+            padding: 10px;
+            }
+            QPushButton:hover {
+                background-color: #0578eb;
+                }
+        """)
+
+        self.figure, self.ax = plt.subplots(3, 2, figsize=(12, 18))
+
+        self.canvas = FigureCanvas(self.figure)
+        self.layout.addWidget(NavigationToolbar(self.canvas, self))
+        self.layout.addWidget(self.canvas)
+
+        self.load_and_process_data()
+        self.filtered_df = self.df
+        self.plot_data()
+
+    def load_and_process_data(self):
+        try:
+            # créer un moteur de base de données
+            engine = create_engine('postgresql://postgres:mellowo@localhost:5432/bi')
+
+            # lire les données de la base de données dans un DataFrame
+            self.df = pd.read_sql('SELECT * FROM dbbi', engine)
+
+            # afficher les noms des colonnes et quelques lignes pour vérification
+            print("Column names:", self.df.columns)
+            print("Sample data:\n", self.df.head())
+
+            if 'nom' in self.df.columns:
+                noms = self.df['nom'].dropna().unique()  # supprimer les valeurs NaN et obtenir les noms uniques
+                cleaned_noms = [nom.strip() for nom in noms] # supprimer les espaces au début/à la fin
+                unique_noms = sorted(set(cleaned_noms)) # supprimer les doublons et trier
+                self.name_listwidget.clear()  # vider les éléments existants
+                self.name_listwidget.addItems(["Tous les noms"] + unique_noms)
+            else:
+                print("Column 'nom' does not exist in the DataFrame.")
+
+            # s'assurer que la colonne 'date' est au format datetime
+            if 'date' in self.df.columns:
+                self.df['date'] = pd.to_datetime(self.df['date'], errors='coerce')
+                self.df['month'] = self.df['date'].dt.month
+
+            # fonction pour obtenir le jour de la semaine à partir d'un objet datetime
+                def get_day_of_week(date_obj):
+                    return date_obj.strftime("%A") if pd.notna(date_obj) else None
+
+                # appliquer la fonction à la colonne 'date' et créer une nouvelle colonne 'Jour_de_la_Semaine'
+                self.df['Jour_de_la_Semaine'] = self.df['date'].apply(get_day_of_week)
+
+                # convertir 'travail' en format numérique pour l'affichage
+                self.df['travail'] = pd.to_timedelta(self.df['travail']).dt.total_seconds() / 3600  # convertir en heures
+            else:
+                print("Column 'date' does not exist in the DataFrame.")
+
+        except Exception as e:
+            print(f"Erreur lors du chargement et du traitement des données : {e}")
+            self.df = pd.DataFrame()  # définir sur DataFrame vide en cas d'erreur
+
+    def apply_filter(self):
+        try:
+            # Print initial DataFrame
+            print("Initial DataFrame:\n", self.df.head())
+
+            selected_months = []
+            selected_items = self.month_listwidget.selectedItems()
+
+            # Check if 'tous les mois' is selected
+            if any(item.text() == 'tous les mois' for item in selected_items):
+                selected_months.append('tous les mois')
+            else:
+                # Convert numeric month values
+                selected_months = [int(item.text()) for item in selected_items if item.text().isdigit()]
+            selected_names = [item.text() for item in self.name_listwidget.selectedItems()]
+
+            print("Selected months:", selected_months)
+            print("Selected names:", selected_names)
+
+
+            print("Selected months:", selected_months)
+            print("Selected names:", selected_names)
+
+            if 'date' in self.df.columns:
+                # Filter by selected month
+                if selected_months:
+                    month_filtered_df = self.df[self.df['date'].dt.month.isin(selected_months)]
+                else:
+                    month_filtered_df = self.df
+
+                # Filter by selected name
+                if "Tous les noms" not in selected_names:
+                    self.filtered_df = month_filtered_df[month_filtered_df['nom'].isin(selected_names)]
+                else:
+                    self.filtered_df = month_filtered_df
+
+                # Clear the current QListWidget items
+                self.list_widget.clear()
+
+                # Populate the QListWidget with filtered data
+                for _, row in self.filtered_df.iterrows():
+                    item_text = f"{row['nom']} - {row['date']}"  # Customize this to display what you want
+                    list_item = QListWidgetItem(item_text)
+                    self.list_widget.addItem(list_item)
+
+                self.list_widget.clear()
+
+                # Populate the QListWidget with filtered data
+                for _, row in self.filtered_df.iterrows():
+                    item_text = f"{row['nom']} - {row['date']}"  # Customize this to display what you want
+                    list_item = QListWidgetItem(item_text)
+                    self.list_widget.addItem(list_item)
+
+                print("Filtered DataFrame:\n", self.filtered_df)
+
+            else:
+                print("Column 'date' does not exist in the DataFrame.")
+                self.filtered_df = pd.DataFrame()
+
+            self.plot_data()
+
+        except Exception as e:
+            print(f"Erreur lors de l'application du filtre : {e}")
+            self.filtered_df = pd.DataFrame() 
+
     def plot_data(self):
         if self.filtered_df.empty:
             # effacer les graphiques précédents
@@ -179,42 +347,52 @@ class Dashboard(QWidget):
         grouped.index = weekdays_fr 
 
         self.ax[1, 0].clear()
-        bars = self.ax[1, 0].bar(grouped.index, grouped.values, color='skyblue')
+        sns.barplot(x=grouped.index, y=grouped.values, palette='Blues_d', ax=self.ax[1, 0])
 
-        # mettre en surbrillance les valeurs les plus élevées et les plus basses
-        highest_day = grouped.idxmax()
-        lowest_day = grouped.idxmin()
-        self.ax[1, 0].bar(highest_day, grouped.max(), color='green', edgecolor='black')
-        self.ax[1, 0].bar(lowest_day, grouped.min(), color='red', edgecolor='black')
-        
+        # **Check if `grouped` contains data before highlighting bars**
+        if len(grouped) > 0:
+            # Ensure indices are within range
+            highest_day = grouped.idxmax()
+            lowest_day = grouped.idxmin()
+            try:
+                highest_idx = grouped.index.get_loc(highest_day)
+                lowest_idx = grouped.index.get_loc(lowest_day)
+                self.ax[1, 0].patches[highest_idx].set_facecolor('green')
+                self.ax[1, 0].patches[lowest_idx].set_facecolor('red')
+            except IndexError:
+                pass  # Handle the case where indices are out of range
+
         self.ax[1, 0].set_xlabel('Jour de la Semaine')
         self.ax[1, 0].set_ylabel('Moyenne Travail (heures)')
         self.ax[1, 0].set_title('Moyenne Travail par Jour de la Semaine')
 
-        # ajouter des infobulles interactives  pour le graphique en barres
-        mplcursors.cursor(bars, hover=True).connect(
-            "add", lambda sel: sel.annotation.set_text(f'{grouped.index[sel.index]}: {grouped.values[sel.index]:.1f} heures')
-        )
+        # **Ensure `patches` list is populated before adding interactive tooltips**
+        if hasattr(self.ax[1, 0], 'patches') and len(self.ax[1, 0].patches) > 0:
+            mplcursors.cursor(self.ax[1, 0].patches, hover=True).connect(
+                "add", lambda sel: sel.annotation.set_text(f'{grouped.index[sel.index]}: {grouped.values[sel.index]:.1f} heures')
+            )
 
         # tracer le travail pour chaque date, y compris les dates manquantes
         all_dates = pd.date_range(start=self.filtered_df['date'].min(), end=self.filtered_df['date'].max())
         full_df = pd.DataFrame({'date': all_dates})
         full_df = full_df.merge(self.filtered_df[['date', 'travail']], on='date', how='left').fillna({'travail': 0})
-        self.ax[1, 1].clear()
-        line = self.ax[1, 1].plot(full_df['date'], full_df['travail'], marker='o', color='blue')[0]
 
-        # mettre en surbrillance les valeurs les plus élevées et les plus basses dans le graphique en ligne
-        avg_value = full_df['travail'].mean()
-        max_date = full_df.loc[full_df['travail'] == full_df['travail'].max(), 'date'].iloc[0]
-        min_date = full_df.loc[full_df['travail'] == full_df['travail'].min(), 'date'].iloc[0]
-        self.ax[1, 1].plot(max_date, full_df['travail'].max(), 'go', markersize=10)  
-        self.ax[1, 1].plot(min_date, full_df['travail'].min(), 'ro', markersize=10) 
-        
+        self.ax[1, 1].clear()
+        sns.lineplot(x='date', y='travail', data=full_df, marker='o', color='blue', ax=self.ax[1, 1])
+
+        # **Check if `full_df` contains data before plotting points in the line chart**
+        if not full_df.empty:
+            avg_value = full_df['travail'].mean()
+            max_date = full_df.loc[full_df['travail'] == full_df['travail'].max(), 'date'].iloc[0]
+            min_date = full_df.loc[full_df['travail'] == full_df['travail'].min(), 'date'].iloc[0]
+            self.ax[1, 1].plot(max_date, full_df['travail'].max(), 'go', markersize=10)
+            self.ax[1, 1].plot(min_date, full_df['travail'].min(), 'ro', markersize=10)
+
         # changer la couleur de la ligne pour indiquer le seuil
         self.ax[1, 1].plot(full_df['date'], full_df['travail'], color='blue')
         self.ax[1, 1].axhline(y=8, color='red', linestyle='--', label='Seuil de 8 heures')
         self.ax[1, 1].axhline(y=avg_value, color='orange', linestyle='--', label='Moyenne Travail')
-    
+
         self.ax[1, 1].set_xlabel('Date')
         self.ax[1, 1].set_ylabel('Travail (heures)')
         self.ax[1, 1].set_title('Travail au Fil du Temps')
@@ -225,22 +403,42 @@ class Dashboard(QWidget):
         monthly_totals.index = monthly_totals.index.to_timestamp()
 
         self.ax[2, 0].clear()
-        bars = self.ax[2, 0].bar(monthly_totals.index.strftime('%Y-%m'), monthly_totals['travail'], color='skyblue')
+        sns.barplot(x=monthly_totals.index.strftime('%Y-%m'), y=monthly_totals['travail'], palette='Blues_d', ax=self.ax[2, 0])
 
         self.ax[2, 0].set_xlabel('Mois')
         self.ax[2, 0].set_ylabel('Moyenne Travail (heures)')
         self.ax[2, 0].set_title('Moyenne Travail par Mois')
 
+        # **Ensure `patches` list is populated before highlighting bars**
+        if len(self.ax[2, 0].patches) > 0:
+            highest_day = grouped.idxmax()
+            lowest_day = grouped.idxmin()
+            try:
+                highest_idx = grouped.index.get_loc(highest_day)
+                lowest_idx = grouped.index.get_loc(lowest_day)
+                self.ax[2, 0].patches[highest_idx].set_facecolor('green')
+                self.ax[2, 0].patches[lowest_idx].set_facecolor('red')
+            except IndexError:
+                pass  # Handle the case where indices are out of range
+
         # ajouter des infobulles interactives pour le graphique en barres
-        mplcursors.cursor(bars, hover=True).connect(
-            "add", lambda sel: sel.annotation.set_text(f'{monthly_totals.index[sel.index].strftime("%Y-%m")}: {monthly_totals["travail"].iloc[sel.index]:.1f} heures')
-        )
+        if hasattr(self.ax[2, 0], 'patches') and len(self.ax[2, 0].patches) > 0:
+            mplcursors.cursor(self.ax[2, 0].patches, hover=True).connect(
+                "add", lambda sel: sel.annotation.set_text(f'{monthly_totals.index[sel.index].strftime("%Y-%m")}: {monthly_totals["travail"].iloc[sel.index]:.1f} heures')
+            )
 
         # tracer le nombre de jours de travail par mois
         days_of_travail = monthly_totals['travail'] / 8  # Convertir les heures en jours
 
         self.ax[2, 1].clear()
-        line = self.ax[2, 1].plot(monthly_totals.index, days_of_travail, marker='o', color='blue')[0]
+        sns.lineplot(x=monthly_totals.index, y=days_of_travail, marker='o', color='blue', ax=self.ax[2, 1])
+
+        max_days = days_of_travail.max()
+        min_days = days_of_travail.min()
+        max_date_days = days_of_travail.idxmax()
+        min_date_days = days_of_travail.idxmin()
+        self.ax[2, 1].plot(max_date_days, max_days, 'go', markersize=10, label='Max Jours de Travail')
+        self.ax[2, 1].plot(min_date_days, min_days, 'ro', markersize=10, label='Min Jours de Travail')
 
         self.ax[2, 1].set_xlabel('Mois')
         self.ax[2, 1].set_ylabel('Nombre de Jours de Travail')
@@ -252,27 +450,19 @@ class Dashboard(QWidget):
         total_weeks = len(pd.date_range(start=self.filtered_df['date'].min(), end=self.filtered_df['date'].max(), freq='W'))
 
         expected_hours = expected_hours_per_week * total_weeks
-
-        # s'assurer que les valeurs sont non négatives
         actual_hours = max(0, actual_hours)
         expected_hours = max(0, expected_hours)
 
         self.ax[0, 1].clear()
-        if expected_hours > 0: 
-            self.ax[0, 1].pie(
+        if expected_hours > 0:
+            wedges, texts, autotexts = self.ax[0, 1].pie(
                 [actual_hours, expected_hours - actual_hours],
                 labels=['Heures Réelles', 'Heures Attendues'],
                 autopct='%1.1f%%',
                 startangle=90,
-                colors=['#ff9999', '#66b3ff'],
-                wedgeprops=dict(width=0.3)  # transformer le graphique circulaire en graphique en anneau
+                colors=['#1887f5', '#fa0707'],
+                wedgeprops=dict(width=0.3) 
             )
-            self.ax[0, 1].set_title('Heures Réelles vs Heures Attendues')
-        else:
-            self.ax[0, 1].text(0.5, 0.5, 'Pas d\'heures attendues à afficher',
-                                horizontalalignment='center', verticalalignment='center',
-                                transform=self.ax[2, 0].transAxes, fontsize=12, color='red')
-
 
         if 'nom' in self.filtered_df.columns:
             employee_totals = self.filtered_df.groupby('nom')['travail'].sum()
@@ -288,7 +478,18 @@ class Dashboard(QWidget):
                             transform=self.ax[0, 0].transAxes, fontsize=12, color='red')
             self.ax[0, 0].set_title('Employés Travaillés')
 
-            self.ax[0, 0].axis('off')
+            self.ax[0, 0].axis('off')            
+
+        self.ax[0, 0].axis('off')
+
+        plt.subplots_adjust(wspace=0.5, hspace=0.5)
+        self.canvas.draw()
+
+if __name__ == "__main__":
+    app = QWidget(sys.argv)
+    window = Dashboard ()
+    window.showMaximized()
+    sys.exit(app.exec_())
 
         plt.subplots_adjust(wspace=0.5, hspace=0.5)
         self.canvas.draw()
